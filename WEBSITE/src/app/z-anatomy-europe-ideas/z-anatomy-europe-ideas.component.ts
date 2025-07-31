b4308e228fec16c9f30286ececb4159f3ad4335d
moueza <221372+moueza@users.noreply.github.com>
2025-07-31 15:39:40 +0200

    Chore: Angular v18 -> v19
    ng update

--- WEBSITE/src/app/z-anatomy-europe-ideas/z-anatomy-europe-ideas.component.ts
+++ WEBSITE/src/app/z-anatomy-europe-ideas/z-anatomy-europe-ideas.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-z-anatomy-europe-ideas',
-  standalone: true,
-  imports: [],
-  templateUrl: './z-anatomy-europe-ideas.component.html',
-  styleUrl: './z-anatomy-europe-ideas.component.scss'
+    selector: 'app-z-anatomy-europe-ideas',
+    imports: [],
+    templateUrl: './z-anatomy-europe-ideas.component.html',
+    styleUrl: './z-anatomy-europe-ideas.component.scss'
 })
 export class ZAnatomyEuropeIdeasComponent {
 
